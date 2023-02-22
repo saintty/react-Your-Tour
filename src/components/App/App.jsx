130d6b16295e81adaed00ff243a5956919d311b2
Artyom Uzinger <aauzinger@yandex.ru>
2023-02-22 19:07:30 +0300

    Add styles: common application style settings

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import "./App.module.scss";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 };
 
 export default App;
