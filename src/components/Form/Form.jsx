c22419e6753154c580cf0049475312e13ec46c06
Artyom Uzinger <aauzinger@yandex.ru>
2023-02-24 15:49:14 +0300

    Add form: select input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Section from "../../UI/section/Section";
+import Select from "../../UI/select/Select";
 
 import styles from "./Form.module.scss";
+import { options } from "./selectOptions";
 
 const Form = () => {
   return (
     <Section
       title="Собери свой тур"
       subtitle="Идейные соображения высшего порядка,а также рамки и место обучения кадров"
-    ></Section>
+    >
+      <Select
+        id="destination"
+        label="Направление"
+        required={true}
+        options={options}
+      />
+    </Section>
   );
 };
 
---
+++ src/components/Form/selectOptions.js
@@ -0,0 +1,19 @@
+export const options = [
+  {
+    name: "Куда хотите ехать",
+    value: "",
+    disabled: true,
+  },
+  {
+    name: "Море",
+    value: "sea",
+  },
+  {
+    name: "Горы",
+    value: "mountain",
+  },
+  {
+    name: "Лес",
+    value: "forest",
+  }
+]
