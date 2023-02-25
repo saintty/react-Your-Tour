c2bfc143a100bdaaa771286bc9e620c9e4ddcbee
Artyom Uzinger <aauzinger@yandex.ru>
2023-02-25 11:49:19 +0300

    Add review structure

--- src/components/Reviews/Review/Review.jsx
+++ src/components/Reviews/Review/Review.jsx
@@ -3,17 +3,15 @@ import ReviewFooter from "../ReviewFooter/ReviewFooter";
 
 import styles from "./Review.module.scss";
 
-const Review = () => {
+const Review = ({ review }) => {
   return (
-    <article classNameName="feedback__item">
-      <p classNameName="feedback__text">
-        Идейные соображения высшего порядка, а также рамки и место обучения
-        кадров обеспечивает широкому кругу (специалистов) участие в формировании
-        новых предложений. Идейные соображения высшего порядка, а также начало
-        повседневной работы по формированию позиции позволяет оценить значение
-        модели развития.
-      </p>
-      <ReviewFooter />
+    <article className={styles.item}>
+      <p className={styles.text}>{review.text}</p>
+      <ReviewFooter
+        user={review.user}
+        tour={review.tour}
+        avatar={review.avatar}
+      />
     </article>
   );
 };
