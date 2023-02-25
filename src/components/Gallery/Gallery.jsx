6fb84c1bcca610b3197750e1241a4efeda44622e
Artyom Uzinger <aauzinger@yandex.ru>
2023-02-25 12:22:06 +0300

    Add gallery: section header

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Section from "./../../UI/section/Section";
+
+import styles from "./Gallery.module.scss";
+
+const Gallery = () => {
+  return (
+    <Section
+      title="Фотографии путешествий"
+      subtitle="Идейные соображения высшего порядка, а также рамки и место обучения кадров"
+    ></Section>
+  );
+};
+
+export default Gallery;
