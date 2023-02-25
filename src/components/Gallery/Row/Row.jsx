f5fa13c1b3bb56d2996dc4fb4bbcf5b01ec33c76
Artyom Uzinger <aauzinger@yandex.ru>
2023-02-25 12:41:26 +0300

    Add gallery: picture row

---
+++ src/components/Gallery/Row/Row.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Row.module.scss";
+
+const Row = ({ children }) => {
+  return <div className={styles.row}>{children}</div>;
+};
+
+export default Row;
