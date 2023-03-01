b61e95cdfee18baa393bcf8d19bea4e3c8260d13
Artyom Uzinger <aauzinger@yandex.ru>
2023-03-01 18:25:09 +0300

    Change files: imports

--- src/UI/pageCaption/PageCaption.jsx
+++ src/UI/pageCaption/PageCaption.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "./pageCaption.module.scss";
+import styles from "./PageCaption.module.scss";
 
 const PageCaption = ({ children }) => {
   return <h1 className={styles.title}>{children}</h1>;
