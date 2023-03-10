df1ccd71d3e555c876468e6147ec0921fe1f2779
Artyom Uzinger <aauzinger@yandex.ru>
2023-03-10 18:15:31 +0300

    Add WideContainer: PropTypes

--- src/shared/wideContainer/WideContainer.jsx
+++ src/shared/wideContainer/WideContainer.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import styles from "./WideContainer.scss";
 
 const WideContainer = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
+};
+
+WideContainer.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
 };
 
 export default WideContainer;
