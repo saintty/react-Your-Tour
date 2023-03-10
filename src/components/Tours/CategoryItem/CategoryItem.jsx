1a61efaffb9716c5ccef55f3f0a460cf4ac8796b
Artyom Uzinger <aauzinger@yandex.ru>
2023-03-10 18:57:51 +0300

    Add CategoryItem: PropTypes

--- src/components/Tours/CategoryItem/CategoryItem.jsx
+++ src/components/Tours/CategoryItem/CategoryItem.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import classNames from "classnames";
+import PropTypes from "prop-types";
 
+import cx from "classnames";
 import styles from "./CategoryItem.module.scss";
 
 const CategoryItem = ({ name, link, active, setActive }) => {
   return (
     <li
-      className={classNames(styles.item, active ? styles.item_active : "")}
+      className={cx(styles.item, active ? styles.active : "")}
       onClick={() => setActive(name)}
     >
       <a href={link} className={styles.link}>
@@ -14,6 +15,17 @@ const CategoryItem = ({ name, link, active, setActive }) => {
       </a>
     </li>
   );
+};
+
+CategoryItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  active: PropTypes.bool,
+  setActive: PropTypes.func.isRequired,
+};
+
+CategoryItem.defaultProps = {
+  active: false,
 };
 
 export default CategoryItem;
