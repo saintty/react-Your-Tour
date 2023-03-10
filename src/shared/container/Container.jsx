8b20ec0ff26d5b6dfd7712a26fab6c3f413aaa25
Artyom Uzinger <aauzinger@yandex.ru>
2023-03-10 18:14:40 +0300

    Add Container: PropTypes

--- src/shared/container/Container.jsx
+++ src/shared/container/Container.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./Container.module.scss";
 
 const Container = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
+};
+
+Container.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
 };
 
 export default Container;
