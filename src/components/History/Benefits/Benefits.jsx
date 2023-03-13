b86aa23c9b138c8a14b9e3a734bbd31df3217a67
Artyom Uzinger <aauzinger@yandex.ru>
2023-03-13 17:43:59 +0300

    Add Benefits: PropTypes

--- src/components/History/Benefits/Benefits.jsx
+++ src/components/History/Benefits/Benefits.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./Benefits.module.scss";
 
@@ -12,6 +13,10 @@ const Benefits = ({ benefits }) => {
       ))}
     </ul>
   );
+};
+
+Benefits.propTypes = {
+  benefits: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default Benefits;
